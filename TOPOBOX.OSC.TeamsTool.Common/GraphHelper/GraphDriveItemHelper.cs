c07b07349e3a8621d796cb8ce62fa31cebb92369
Cédric Strini <cedric.strini@geobox.ch>
2022-02-24 17:47:59 +0100

    23.1 - Add GraphDriveItemHelper

---
+++ TOPOBOX.OSC.TeamsTool.Common/GraphHelper/GraphDriveItemHelper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Graph;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TOPOBOX.OSC.TeamsTool.Common.GraphHelper
+{
+    /// <summary>
+    /// GraphDriveItemHelper
+    /// </summary>
+    public sealed class GraphDriveItemHelper
+    {
+        private GraphServiceClient graphClient;
+
+        /// <summary>
+        /// Construtcor
+        /// </summary>
+        /// <param name="graphServiceClient"></param>
+        public GraphDriveItemHelper(GraphServiceClient graphServiceClient)
+        {
+            graphClient = graphServiceClient;
+        }
+
+        /// <summary>
+        /// Get all driveItems from specific user by userId
+        /// </summary>
+        /// <returns>dictionary with driveItemId and Graph.DriveItem Objects</returns>
+        public Dictionary<string, DriveItem> GetDriveItems(string userId)
+        {
+            var graphRequest = graphClient.Users[userId].Drive.Root.Children.Request();
+
+            var task = new Task<Dictionary<string, DriveItem>>(gR =>
+            {
+                var request = gR as DriveItemChildrenCollectionRequest;
+                var answer = request.GetAsync().Result;
+
+                if (answer.Any())
+                {
+                    Dictionary<string, DriveItem> returnDict = new Dictionary<string, DriveItem>();
+                    foreach (var driveItem in answer)
+                    {
+                        returnDict.Add(driveItem.Id, driveItem);
+                    }
+                    return returnDict;
+                }
+
+                return new Dictionary<string, DriveItem>();
+            }, graphRequest);
+
+            task.Start();
+            return task.Result;
+        }
+    }
+}
